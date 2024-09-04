2f90d25a5dea78205d66b19891e1df8edee80de3
Andres_Prado_Morgaz <andres.prado.external@eviden.com>
2024-09-04 21:31:16 +0200

    Board svg now working, need to redo the logic.

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react'
+import { useState } from 'react'
 import './App.css'
 import { Board } from './components/Board/Board.jsx'
 import { Info } from './components/Info/Info.jsx'
@@ -11,56 +11,37 @@ function App () {
   // Cuando se le da a tirar el dado, aparece en el centro de colorHome animación: varios random durante x tiempo, cambiando cada x tiempo y luego se fija uno y cambia de color.
 
   // Falta ver en el tema posicionamiento la casuística de dos fichas en la misma casilla, y de las casillas centrales.
+  // El array de posiciones será así:
+  // [{ tile: 'tile1', topSideA: 22, leftSideA: 170, topSideB: 22, leftSideB: 180 }]
 
   // Guardar turn y pieces en localStorage
   const [pieces, setPieces] = useState([
-    { id: 'bluePiece1', tile: 'tile22', color: 'blue' },
-    { id: 'bluePiece2', tile: 'tile29', color: 'blue' },
-    { id: 'bluePiece3', tile: 'redtile1', color: 'blue' },
-    { id: 'bluePiece4', tile: 'tile46', color: 'blue' },
-    { id: 'redPiece1', tile: 'tile39', color: 'red' },
-    { id: 'redPiece2', tile: 'tile40', color: 'red' },
-    { id: 'redPiece3', tile: 'tile56', color: 'red' },
-    { id: 'redPiece4', tile: 'redtile6', color: 'red' },
-    { id: 'yellowPiece1', tile: 'tile5', color: 'yellow' },
-    { id: 'yellowPiece2', tile: 'tile7', color: 'yellow' },
-    { id: 'yellowPiece3', tile: 'tile12', color: 'yellow' },
-    { id: 'yellowPiece4', tile: 'yellowtile1', color: 'yellow' },
-    { id: 'greenPiece1', tile: 'tile67', color: 'green' },
-    { id: 'greenPiece2', tile: 'tile2', color: 'green' },
-    { id: 'greenPiece3', tile: 'tile4', color: 'green' },
-    { id: 'greenPiece4', tile: 'greentile8', color: 'green' }
+    { id: 'bluePiece1', color: 'blue', tile: 'tile22', side: 'a' },
+    { id: 'bluePiece2', color: 'blue', tile: 'tile29', side: 'a' },
+    { id: 'bluePiece3', color: 'blue', tile: 'redtile1', side: 'a' },
+    { id: 'bluePiece4', color: 'blue', tile: 'tile46', side: 'a' },
+    { id: 'redPiece1', color: 'red', tile: 'tile39', side: 'a' },
+    { id: 'redPiece2', color: 'red', tile: 'tile40', side: 'a' },
+    { id: 'redPiece3', color: 'red', tile: 'tile56', side: 'a' },
+    { id: 'redPiece4', color: 'red', tile: 'redtile6', side: 'a' },
+    { id: 'yellowPiece1', color: 'yellow', tile: 'tile5', side: 'a' },
+    { id: 'yellowPiece2', color: 'yellow', tile: 'tile7', side: 'a' },
+    { id: 'yellowPiece3', color: 'yellow', tile: 'tile12', side: 'a' },
+    { id: 'yellowPiece4', color: 'yellow', tile: 'yellowtile1', side: 'a' },
+    { id: 'greenPiece1', color: 'green', tile: 'tile67', side: 'a' },
+    { id: 'greenPiece2', color: 'green', tile: 'tile2', side: 'a' },
+    { id: 'greenPiece3', color: 'green', tile: 'tile4', side: 'a' },
+    { id: 'greenPiece4', color: 'green', tile: 'greentile8', side: 'a' }
   ])
-
-  const [positions, setPositions] = useState([])
 
   const [diceValue, setDiceValue] = useState(1)
 
-  const calcPositions = () => {
-    const newPositions = pieces.map(piece => {
-      const tile = document.getElementById(piece.tile)
-      const rect = tile.getBoundingClientRect()
-      return {
-        id: piece.id,
-        color: piece.color,
-        top: rect.top + window.scrollY,
-        left: rect.left + window.scrollX
-      }
-    })
-    setPositions(newPositions)
-  }
-
-  useEffect(() => {
-    calcPositions()
-    window.addEventListener('resize', calcPositions)
-
-    return () => window.removeEventListener('resize', calcPositions)
-  // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [pieces])
-
-  const updatePiecePosition = (pieceId, newTile) => {
+  // REVISAR ESTE
+  const updatePieces = (pieceId, newTile) => {
     setPieces((prevPieces) => {
       const index = prevPieces.findIndex(piece => piece.id === pieceId)
+
+      // Hay que definir la forma de ver el side al que irá, según esté ocupada ya la casilla o no, después de comprobar que el movimiento es legal, puede ir a side A si está libre o a side B si ya hay una ficha en el A.
 
       const updatedPieces = [
         ...prevPieces.slice(0, index),
@@ -70,10 +51,6 @@ function App () {
 
       return updatedPieces
     })
-  }
-
-  const handleMoveExample = () => {
-    updatePiecePosition('greenPiece1', 'bluetile1')
   }
 
   const handleNextTurn = () => {
@@ -106,7 +83,6 @@ function App () {
         <div>
           <h1>PARCHÍS</h1>
           <button onClick={handleNextTurn}>Siguiente jugador</button>
-          <button onClick={handleMoveExample}>Ejemplo: Mover ficha verde 1</button>
           <button style={{ float: 'right', marginRight: 0 }}>Reiniciar partida</button>
           <br />
           <button onClick={handleRollDice}>Tirar el dado</button>
@@ -116,14 +92,15 @@ function App () {
         </Board>
         <Info turn={turn} />
       </div>
-      {positions.map(position =>
+      {/* Este mapeo está mal, tengo que buscar la posición de la pieza (tile y position 1 o 2, o 0 o 1, o true o false) y buscar los % en el array de posiciones que está pendiente de hacer */}
+      {/* pieces.map(position =>
         <Piece
           key={position.id}
           id={position.id}
           color={position.color}
           top={position.top}
           left={position.left}
-        />)}
+        />) */}
     </>
   )
 }
--- src/components/Board/Board.jsx
+++ src/components/Board/Board.jsx
@@ -1,137 +1,42 @@
+import { useEffect, useRef } from 'react'
 import { ColorHome } from '../ColorHome/ColorHome'
 import './Board.css'
 
 export const Board = ({ children, turn }) => {
+  const boardRef = useRef(null)
+
+  useEffect(() => {
+    const handleResize = () => {
+      const isWiderThanTaller = window.innerWidth > window.innerHeight
+
+      if (boardRef.current) {
+        if (isWiderThanTaller) {
+          boardRef.current.classList.add('wide')
+        } else {
+          boardRef.current.classList.remove('wide')
+        }
+      }
+    }
+
+    // Initial check
+    handleResize()
+
+    // Add event listener for window resize
+    window.addEventListener('resize', handleResize)
+
+    // Cleanup event listener on component unmount
+    return () => {
+      window.removeEventListener('resize', handleResize)
+    }
+  }, [])
   return (
-    <div className='board-container'>
+    <div ref={boardRef} className='board-container'>
       <div className='board'>
         <ColorHome color='red' turn={turn === 'red'} />
-        <div className='quadrant' id='topPathQuadrant'>
-          <div className='tile' id='tile35' />
-          <div className='tile redtile safe' id='redtile1' />
-          <div className='tile' id='tile33' />
-          <div className='tile' id='tile36' />
-          <div className='tile redtile' id='redtile2' />
-          <div className='tile' id='tile32' />
-          <div className='tile' id='tile37' />
-          <div className='tile redtile' id='redtile3' />
-          <div className='tile' id='tile31' />
-          <div className='tile' id='tile38' />
-          <div className='tile redtile' id='redtile4' />
-          <div className='tile' id='tile30' />
-          <div className='tile redtile' id='tile39' />
-          <div className='tile redtile' id='redtile5' />
-          <div className='tile safe' id='tile29' />
-          <div className='tile' id='tile40' />
-          <div className='tile redtile' id='redtile6' />
-          <div className='tile' id='tile28' />
-          <div className='tile' id='tile41' />
-          <div className='tile redtile' id='redtile7' />
-          <div className='tile' id='tile27' />
-        </div>
         <ColorHome color='blue' turn={turn === 'blue'} />
-        <div className='quadrant' id='leftPathQuadrant'>
-          <div className='tile' id='tile50' />
-          <div className='tile' id='tile49' />
-          <div className='tile' id='tile48' />
-          <div className='tile' id='tile47' />
-          <div className='tile safe' id='tile46' />
-          <div className='tile' id='tile45' />
-          <div className='tile' id='tile44' />
-          <div className='tile greentile safe' id='greentile1' />
-          <div className='tile greentile' id='greentile2' />
-          <div className='tile greentile' id='greentile3' />
-          <div className='tile greentile' id='greentile4' />
-          <div className='tile greentile' id='greentile5' />
-          <div className='tile greentile' id='greentile6' />
-          <div className='tile greentile' id='greentile7' />
-          <div className='tile' id='tile52' />
-          <div className='tile' id='tile53' />
-          <div className='tile' id='tile54' />
-          <div className='tile' id='tile55' />
-          <div className='tile greentile' id='tile56' />
-          <div className='tile' id='tile57' />
-          <div className='tile' id='tile58' />
-        </div>
-        <div className='quadrant' id='centerQuadrant'>
-          <div id='centerTop'>
-            <div className='tile' id='tile42' />
-            <div className='tile redtile' id='redtile8' />
-            <div className='tile' id='tile26' />
-          </div>
-          <div id='centerBot'>
-            <div className='tile' id='tile60' />
-            <div className='tile yellowtile' id='yellowtile8' />
-            <div className='tile' id='tile8' />
-          </div>
-          <div id='centerLeft'>
-            <div className='tile' id='tile43' />
-            <div className='tile greentile' id='greentile8' />
-            <div className='tile' id='tile59' />
-          </div>
-          <div id='centerRight'>
-            <div className='tile' id='tile25' />
-            <div className='tile bluetile' id='bluetile8' />
-            <div className='tile' id='tile9' />
-          </div>
-          <div id='centerCenter'>
-            <div className='redtile' id='redTriangleTile' />
-            <div className='yellowtile' id='yellowTriangleTile' />
-            <div className='greentile' id='greenTriangleTile' />
-            <div className='bluetile' id='blueTriangleTile' />
-          </div>
-        </div>
-        <div className='quadrant' id='rightPathQuadrant'>
-          <div className='tile' id='tile24' />
-          <div className='tile' id='tile23' />
-          <div className='tile bluetile' id='tile22' />
-          <div className='tile' id='tile21' />
-          <div className='tile' id='tile20' />
-          <div className='tile' id='tile19' />
-          <div className='tile' id='tile18' />
-          <div className='tile bluetile' id='bluetile7' />
-          <div className='tile bluetile' id='bluetile6' />
-          <div className='tile bluetile' id='bluetile5' />
-          <div className='tile bluetile' id='bluetile4' />
-          <div className='tile bluetile' id='bluetile3' />
-          <div className='tile bluetile' id='bluetile2' />
-          <div className='tile bluetile safe' id='bluetile1' />
-          <div className='tile' id='tile10' />
-          <div className='tile' id='tile11' />
-          <div className='tile safe' id='tile12' />
-          <div className='tile' id='tile13' />
-          <div className='tile' id='tile14' />
-          <div className='tile' id='tile15' />
-          <div className='tile' id='tile16' />
-        </div>
         <ColorHome color='green' turn={turn === 'green'} />
-        <div className='quadrant' id='botPathQuadrant'>
-          <div className='tile' id='tile61' />
-          <div className='tile yellowtile' id='yellowtile7' />
-          <div className='tile' id='tile7' />
-          <div className='tile' id='tile62' />
-          <div className='tile yellowtile' id='yellowtile6' />
-          <div className='tile' id='tile6' />
-          <div className='tile safe' id='tile63' />
-          <div className='tile yellowtile' id='yellowtile5' />
-          <div className='tile yellowtile' id='tile5' />
-          <div className='tile' id='tile64' />
-          <div className='tile yellowtile' id='yellowtile4' />
-          <div className='tile' id='tile4' />
-          <div className='tile' id='tile65' />
-          <div className='tile yellowtile' id='yellowtile3' />
-          <div className='tile' id='tile3' />
-          <div className='tile' id='tile66' />
-          <div className='tile yellowtile' id='yellowtile2' />
-          <div className='tile' id='tile2' />
-          <div className='tile' id='tile67' />
-          <div className='tile yellowtile safe' id='yellowtile1' />
-          <div className='tile' id='tile1' />
-        </div>
         <ColorHome color='yellow' turn={turn === 'yellow'} />
         {children}
-        <div className='prueba' />
-        <div className='prueba2' />
       </div>
     </div>
   )
